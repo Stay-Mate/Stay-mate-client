a4b3bc1a9c336fd3ddfcd9720f15a717a9811efc
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-04-24 19:36:59 +0900

    💄 :: 회원가입 form 디자인, ✨ :: 회원가입 성공 시 login 페이지로 리다이렉팅

--- components/Signup/index.tsx
+++ components/Signup/index.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import React, { FormEvent, useState } from "react";
 
 const Signup = () => {
-  const [name, setName ] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [room_number, setRoomNumber] = useState("");
@@ -16,7 +16,7 @@ const Signup = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name, email, password, room_number}),
+      body: JSON.stringify({ name, email, password, room_number }),
     });
 
     const data = await response.json();
@@ -26,7 +26,7 @@ const Signup = () => {
       // JWT 토큰을 클라이언트에 저장하는 코드 작성
       localStorage.setItem("token", data.token);
       console.log("success");
-      router.push("/");
+      router.push("/login");
     } else {
       // 회원가입 실패 처리
       console.log("Sorry");
@@ -40,49 +40,54 @@ const Signup = () => {
         <div className="w-[400px] h-[230px] ml-[300px] mt-[130px] text-center ">
           <h2 className="font-normal text-[#EA7589] font-mono">SIGN UP</h2>
           <form
-            className="w-full h-full bg-slate-500] mt-[150px]"
+            className="w-full h-full bg-slate-500] mt-[80px]"
             onSubmit={handleSubmit}
           >
-            <div >
-              <div>name</div>
+            <div className="flex flex-col mb-4">
+              <div className="mr-96 text-gray-500">name</div>
               <input
+                className="pt-3 pb-2 outline-none border-b-2 text-gray-600"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                placeholder="이메일을 입력해주세요"
+                placeholder="이름을 입력해주세요."
                 required
               />
             </div>
-            <div >
-              <div >email</div>
+            <div className="flex flex-col mb-4">
+              <div className="mr-96 text-gray-500">email</div>
               <input
+                className="pt-3 pb-2 outline-none border-b-2 text-gray-600"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="이메일을 입력해주세요"
                 required
               />
             </div>
-            <div>
-              <div >password</div>
+            <div className="flex flex-col mb-4">
+              <div className="mr-96 text-gray-500">password</div>
               <input
+                className="pt-3 pb-2 outline-none border-b-2 text-gray-600"
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                placeholder="비밀번호를 입력해주세요"
+                placeholder="비밀번호를 입력해주세요."
                 required
               />
             </div>
-            <div>
-              <div>Room</div>
+            <div className="flex flex-col mb-4">
+              <div className="mr-96 text-gray-500">Room</div>
               <input
+                className="pt-3 pb-2 outline-none border-b-2 text-gray-600"
                 value={room_number}
                 onChange={(e) => {
-                  setRoomNumber(e.target.value)
+                  setRoomNumber(e.target.value);
                 }}
-                placeholder="이메일을 입력해주세요"
+                placeholder="호실을 입력해주세요. ex. 111"
                 required
               />
             </div>
             <button
+              className="w-[140px] h-[40px] uppercase bg-[#ff9391] text-[#fff]"
               type="submit"
             >
               sign up
