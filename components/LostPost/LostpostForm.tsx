d6ec90c297603ea4bb6af63a6dc4d192fc045c4e
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-05-29 00:04:05 +0900

    :art: :: 데이터 넘기는 방식 개선

--- components/LostPost/LostpostForm.tsx
+++ components/LostPost/LostpostForm.tsx
@@ -1,30 +1,63 @@
-import React, { useState } from "react";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  MouseEvent,
+  useEffect,
+  useState,
+} from "react";
 import NavBar from "../NavBar";
+import { useRouter } from "next/router";
+import { PostType } from "@/types/post.type";
 
 const LostpostForm = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [post, setPost] = useState<PostType>({
+    title: "",
+    content: "",
+    imgUrl: "",
+    createdAt: "",
+  });
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const [error, setError] = useState<string>("");
 
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("content", content);
-    formData.append("image_url", imageUrl);
-
-    try {
-      const response = await fetch("/api/post", {
-        method: "POST",
-        body: formData,
-      });
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
+  const handleImgChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    const imgUrl = URL.createObjectURL(file as Blob);
+    setPost((prevPost) => ({
+      ...prevPost,
+      imgUrl: imgUrl,
+    }));
   };
+
+  const router = useRouter();
+
+  const handleCancel = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!post.title && !post.content && !post.imgUrl) {
+      setError("모든 항목은 필수 항목입니다.");
+      console.log("FAIL");
+      return;
+    } else {
+      // TODO: Add logic to handle form submission
+      console.log("SUCCESS", post);
+    }
+
+    setError(""); // 에러 초기화
+  };
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString(); // 형식을 맞추어야 할 경우 필요한 포맷으로 변환할 수 있습니다.
+    setPost((prevPost) => ({
+      ...prevPost,
+      createdAt: formattedDate,
+    }));
+  }, []);
 
   return (
     <>
@@ -37,24 +70,36 @@ const LostpostForm = () => {
           <input
             className="w-[800px] h-[50px] outline-none text-2xl text-[#766D6D] ml-10 mt-5 p-2 border-b-2"
             placeholder="제목"
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
+            value={post.title}
+            onChange={(e) =>
+              setPost((prevPost) => ({
+                ...prevPost,
+                title: e.target.value,
+              }))
+            }
           />
           <br />
           <input
             className="w-[800px] h-[40px] outline-none text-xl text-[#766D6D] ml-10 mt-5 p-2 border-b-2"
             placeholder="내용"
-            value={content}
-            onChange={(event) => setContent(event.target.value)}
+            value={post.content}
+            onChange={(e) =>
+              setPost((prevPost) => ({
+                ...prevPost,
+                content: e.target.value,
+              }))
+            }
           />
+          <br />
           <div className="w-[800px] h-[300px] bg-[#F6EBEB] ml-10 mt-4 pt-[120px] pl-[300px]">
-            <input
-              type="file"
-              accept="image/*"
-            />
+            <input type="file" accept="image/*" onChange={handleImgChange} />
           </div>
+          {/* {error && <p style={{ color: "red" }}>{error}</p>} */}
           <div className="w-[210px] h-[40px] mt-[60px] ml-[330px] flex justify-between">
-            <button className="w-[100px] h-full bg-[#FFB4B4] text-[#fff]">
+            <button
+              className="w-[100px] h-full bg-[#FFB4B4] text-[#fff]"
+              onClick={handleCancel}
+            >
               취소
             </button>
             <button
---
+++ types/post.type.ts
@@ -0,0 +1,6 @@
+export interface PostType {
+  title: string;
+  content: string;
+  imgUrl: string;
+  createdAt: string;
+}
