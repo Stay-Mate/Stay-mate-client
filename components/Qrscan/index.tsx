d3ab638f1eb86db91179533b76858362cd8327b9
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-07-04 14:19:17 +0900

    FEAT : 스캔한 코드 서버로 넘기기

--- components/Qrscan/index.tsx
+++ components/Qrscan/index.tsx
@@ -1,3 +1,4 @@
+import axios, { AxiosResponse } from "axios";
 import React, { useState } from "react";
 import { QrReader } from "react-qr-reader";
 
@@ -5,6 +6,35 @@ export const Qrscan = () => {
   const [selected, setSelected] = useState<string>("environment");
   const [startScan, setStartScan] = useState<boolean>(false);
   const [data, setData] = useState<string>("");
+
+  const handleData = async (data: string) => {
+    try {
+      const response = await fetch("/api/random", {
+        method: "POST",
+        body: JSON.stringify({ qrstring: data }),
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        const { success, key, time } = result;
+
+        if (success) {
+          // Update the state or perform any other actions with the received data
+          console.log("Key:", key);
+          console.log("Time:", time);
+        } else {
+          console.error("An error occurred:", result.error);
+        }
+      } else {
+        console.error("HTTP error:", response.status);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
     <div className="text-center">
@@ -18,7 +48,7 @@ export const Qrscan = () => {
       </button>
 
       {startScan && (
-        <div >
+        <div>
           <select onChange={(e) => setSelected(e.target.value)}>
             <option value={"environment"}>후면 카메라</option>
             <option value={"user"}>전면 카메라</option>
@@ -27,9 +57,12 @@ export const Qrscan = () => {
             scanDelay={1000}
             constraints={{ facingMode: selected }} // chooseDeviceId={()=>selected}
             onResult={(result, error) => {
+              // lodash debounce or throttle
+              // console.info(result, error);
               if (result) {
-                setData(result?.getText());
-                console.log(`loaded >>>`, result);
+                setData(result.getText());
+                console.log(`loaded >>>`, result.getText());
+                handleData(result.getText());
               }
               if (!!error) {
                 console.log(error);
