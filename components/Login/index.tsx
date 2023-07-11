0330ed91a13ec504805e96d32e4d7a54a8930abc
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-07-11 19:44:37 +0900

    CHORE : 공지 글 작성 내용 부분 수정 input -> textarea

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useLogin } from "@/hooks/useLogin";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Login() {
   const { email, setEmail, password, setPassword, handleSubmit } = useLogin();
