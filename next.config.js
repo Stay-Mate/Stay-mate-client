8dacd4495e786b13d016d3f3a26e31dffaaeac43
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-09-06 15:57:33 +0900

    TEST(api) : 임의로 api url 변경

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://3106.solrukas.me/api/:path*",
+        destination: "http://localhost:8080/api/:path*",
       },
     ];
   },
